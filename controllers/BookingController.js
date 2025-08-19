3d05adac17ab14c31c6caa2b7927a9e93854d70e
Rajkumar Gurjar <mavairk@gmail.com>
2025-08-19 23:47:41 +0530

    second

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -1,17 +1,17 @@
-const Booking = require("../models/booking"); //booking model
+const BookingModel = require("../models/booking"); //booking model
 const Course = require("../models/Course");  //course model
 
 class BookingController {
     static createbooking = async (req,res) => {
         try{
-            const{courseId}=req.params;
+            const {courseId} =req.params;
             const userID = req.user._id; //assume JWT middleware
             const course = await Course.findById(courseId);
 
             if (!course) {
                 return res.status(400).json({message: "Course not found"});
             }
-            const newBooking = await Booking.create({
+            const newBooking = await BookingModel.create({
                 course:course._id,
                 user:userID,
                 price:course.price,
@@ -28,11 +28,11 @@ class BookingController {
     //static arrow function to get user booking
     static getUserBookings= async (req,res) => {
         try{
-            const userID = req.user._id;
-            const booking = await Booking.find({user:userId})
-                .populate("course","title price")
-                .sort({createdAt:-1});
-            return res.status(200).json({booking});
+            const userId = req.user._id;
+            const data = await BookingModel.find({ user:userId })
+            .populate("course","title price")
+            .sort({createdAt:-1});
+            return res.status(200).json({data});
         }catch(error){
             console.error(error);
             return res.status(500).json({message: "Server Error"});
@@ -42,8 +42,8 @@ class BookingController {
     //Static arrow function to cancle booking
     static cancleBooking = async (req,res) => {
         try{
-            const{bookingId}=req.params;
-            const booking = await Booking.findByIdAndUpdate(
+            const { bookingId }=req.params;
+            const booking = await BookingModel.findByIdAndUpdate(
                 bookingId,
                 {status:"Cancelled"},
                 {new: true}
@@ -62,14 +62,14 @@ class BookingController {
     };
     static getAllBookings = async (req,res) => {
         try{
-            const Bookings = await Booking.find()
-                .populate("user","name email")
-                .populate("course","title price")
-                .sort({createdAt:-1});
+            const bookings = await BookingModel.find()
+            .populate("user","name email")
+            .populate("course","title price")
+            .sort({createdAt:-1});
 
             //Format response
             const formatted = bookings.map(b => ({
-                id:b.id,
+                _id:b._id,
                 username:b.user.name,
                 userEmail:b.user.email,
                 courseTitle:b.course.title,
